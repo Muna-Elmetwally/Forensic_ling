2b91a40314bcff681502eea1f973b4b08114cca1
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2024-10-04 11:05:04 +0200

    .

--- forensic_linguistics/src/index.js
+++ forensic_linguistics/src/index.js
@@ -2,4 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <div>
+    <h1>
+      Forensic Linguistics: The Middle East Government Journal on Language and
+      Law
+    </h1>
+  </div>
+);
--- index.js
+++
@@ -1,9 +0,0 @@
-ReactDOM.render(
-  <div>
-    <h1>
-      Forensic Linguistics: The Middle East Government Journal on Language and
-      Law
-    </h1>
-  </div>,
-  document.getElementById("root")
-);
