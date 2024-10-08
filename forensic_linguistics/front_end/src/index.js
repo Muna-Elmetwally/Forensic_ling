2bf9dada3b3634b418dbc18a25a22949048ae5e1
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2024-10-08 05:40:51 +0200

    .

--- forensic_linguistics/front_end/src/index.js
+++ forensic_linguistics/front_end/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "./style.css";
-import "./assets/all.min.css";
+// import "./assets/all.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
