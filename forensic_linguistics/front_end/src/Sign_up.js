50af7bd549fa51ca0ac0cda06391dc360c7fa66c
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2024-10-10 04:35:23 +0300

    set pass limits

--- forensic_linguistics/front_end/src/Sign_up.js
+++ forensic_linguistics/front_end/src/Sign_up.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
 export default function Sign_up() {
+  const [first_name, setFirst_name] = useState("");
+  const [last_name, setLast_name] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeat_password, setRepeat_password] = useState("");
+  const [phone, setPhone] = useState("");
+  const [country, setCountry] = useState("");
+  const [career, setCareer] = useState("");
+  const [accept, setAccept] = useState("fasle");
+
   function submit(e) {
     e.preventDefault();
+    setAccept(true);
   }
+
   return (
     <div className="signup-parent">
       <div className="register">
@@ -13,28 +26,74 @@ export default function Sign_up() {
           style={{ textAlign: "center" }}
         >
           <label htmlfor="first_name">First Name</label>
-          <input id="first_name" type="text" placeholder="First Name" />
-          <label htmlfor="last_name">Last Name</label>
-          <input id="last" type="text" placeholder="Last Name" />
-          <label htmlfor="email">Email</label>
-          <input id="email" type="email" placeholder="Email" required />
-          <label htmlfor="password">Password</label>
-          <input id="pass" type="password" placeholder="Password" />
-          <label htmlfor="confirm_password">Confirm Password</label>
           <input
-            id="fconf_pass"
+            id="first_name"
+            type="text"
+            placeholder="First Name"
+            value={first_name}
+            onChange={(e) => setFirst_name(e.target.value)}
+          />
+          <label htmlfor="last_name">Last Name</label>
+          <input
+            id="last"
+            type="text"
+            placeholder="Last Name"
+            value={last_name}
+            onChange={(e) => setLast_name(e.target.value)}
+          />
+          <label htmlfor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlfor="password">Password</label>
+          <input
+            id="pass"
             type="password"
-            placeholder="Confirm Password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {password.length < 8 && accept && (
+            <p>Password must be at least 8 characters</p>
+          )}
+          <label htmlFor="repeat_password">Repeat Password</label>
+          <input
+            id="repeatpassword"
+            type="password"
+            placeholder="Repeat your Password"
+            value={repeat_password} /* Use the state variable here */
+            onChange={(e) =>
+              setRepeat_password(e.target.value)
+            } /* Update the state on input change */
           />
           <label htmlfor="phone">Phone</label>
-          <input id="phone" type="tel" placeholder="Phone" />
-          <label htmlfor="address">Country</label>
-          <input id="address" type="text" placeholder="Country" />
+          <input
+            id="phone"
+            type="tel"
+            placeholder="Phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+          <label htmlfor="country">Country</label>
+          <input
+            id="country"
+            type="text"
+            placeholder="Country"
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+          />
           <label htmlfor="career">job</label>
           <input
             id="career"
             type="text"
             placeholder="What do you do for living?"
+            value={career}
+            onChange={(e) => setCareer(e.target.value)}
           />
           <div className="button" style={{ textAlign: "center" }}>
             <button id="button">Sign Up</button>
