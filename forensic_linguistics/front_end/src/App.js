35bfe7d3941ddd1c5b962e24e320ba9c6cbe9095
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2024-10-10 04:43:14 +0300

    arranging files

--- forensic_linguistics/front_end/src/App.js
+++ forensic_linguistics/front_end/src/App.js
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Signup from "./Sign_up";
 import Logo from "./components/Logo";
+import Signup from "./Sign_up";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
