d91241378f9d404bc0854735d0ac11013dced484
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2024-10-13 13:17:18 +0300

    styling header and footer

--- forensic_linguistics/front_end/src/components/Footer.js
+++ forensic_linguistics/front_end/src/components/Footer.js
@@ -1,11 +1,13 @@
-// import "./Footer.css"; // Create a CSS file for styles
 import React from "react";
+import "./Footer.css"; // Create a CSS file for styles
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      <p>Contact Us: info@forensiclinguistics.gov </p>
-      <p>© 2024 Forensic Linguistics Authority</p>
-    </footer>
+    <div>
+      <footer className="footer">
+        <p>Contact Us: info@forensiclinguistics.gov </p>
+        <p>© 2024 Forensic Linguistics Authority</p>
+      </footer>
+    </div>
   );
 }
