9db52d62b5ea188896232cdb21b324768c104d88
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2024-10-15 00:30:49 +0300

    .

--- forensic_linguistics/front_end/src/App.js
+++ forensic_linguistics/front_end/src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+// import React from "react";
 // import { Routes, Route } from "react-router-dom";
+import TopBar from "./components/Topbar/TopBar";
 
 export default function App() {
-  return <div className="App">Blog for world wide Forensic Linguists</div>;
+  return <TopBar />;
 }
