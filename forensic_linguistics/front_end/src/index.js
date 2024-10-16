2e800309e9e6bc890939fc33aaae13af52edcec7
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2024-10-16 15:47:16 +0300

    deleting bk end files

--- forensic_linguistics/front_end/src/index.js
+++ forensic_linguistics/front_end/src/index.js
@@ -1,16 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const container = document.getElementById("root");
+const root = createRoot(container); // Create a root
+
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
